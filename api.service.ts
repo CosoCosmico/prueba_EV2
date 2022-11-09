8d9b02c0ede1578e9c1fb23091d3ccbbb1573020
Nico <nico.zuniga@duocuc.cl>
2022-11-09 10:32:42 -0300

    se modifico service

--- api.service.ts
+++ api.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { producto } from '../modelo/producto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  api = `http://localhost:3000/producto`
+
+  constructor(private http:HttpClient) { }
+
+  httpOptions ={
+    headers : new HttpHeaders ({'content-type': 'application/json'})
+  }
+
+  crearProducto(producto:producto):Observable<any>{
+    return this.http.post<producto>(this.api, JSON.stringify, this.httpOptions)
+  }
+
+  listarProductoID(id):Observable<producto[]>{
+    return this.http.get<producto[]>(this.api + '/' + id)
+  }
+
+  listarProducto():Observable<producto[]>{
+    return this.http.get<producto[]>(this.api)
+  }
+
+
 }
